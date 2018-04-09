20349089bc87c7567bf2d0fee4ab519d958e1630
Nico8713 <171wita07@o365.htl-leoben.at>
2018-04-09 16:07:38 +0200

    erstes Commit

---
+++ src/parkplatz.java
@@ -0,0 +1,15 @@
+
+public class parkplatz {
+
+	public static void main(String[] args) {
+		int n = 0;
+		System.out.println("Robert is heite sehr fertig hahahahahha wrestling is zu hort für erm des mocht erm klinisch tod")
+		
+		
+		
+		
+		
+
+	}
+
+}
